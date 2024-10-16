343a412758b88fcb1382a46302daf5abf53492ee
Lucas <lucas.lima@ucl.br>
2024-10-16 01:13:30 -0300

    [Add] Feature

--- Crawl/Crawl/spiders/CrawlVagas.py
+++ Crawl/Crawl/spiders/CrawlVagas.py
@@ -1,5 +1,6 @@
 import requests
 import scrapy
+from datetime import datetime
 
 
 class CrawlvagasSpider(scrapy.Spider):
@@ -8,5 +9,19 @@ class CrawlvagasSpider(scrapy.Spider):
     start_urls = ["https://www.trabalhaes.com.br/vagas-em-vitoria-es/"]
 
     def parse(self, response):
-        for a in response.css('ol.cl-static-search-results a::attr(href)').getall():
-            yield {'link': a}
+        # Cria um arquivo chamado vagas.txt
+        now = datetime.now().strftime("%Y%m%d_%H%M%S")
+        CaminhoRelatorio = f"../../../Relatorio/vagas-{now}.csv"
+        
+        with open(CaminhoRelatorio, "w", encoding="utf-8") as file:
+            file.write("Vaga;Data;Link\n")
+            for vaga in response.css('.list-item'):
+                vaga_info = {
+                    'link': vaga.css('.list-item-title a::attr(href)').get(),
+                    'vaga': vaga.css('.list-item-title a::text').get(),
+                    'data': vaga.css('.list-item-time::text').get()
+                }
+                # como será escrito o arquivo
+                file.write(f"{vaga_info['vaga']};{vaga_info['data']};{vaga_info['link']}\n")# como será escrito o arquivo
+# inicializa o Scrap com o comando: scrapy crawl CrawlVagas
+
